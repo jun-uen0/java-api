70be5af8912bf75e81ae63aed20751c714eb2903
Jun Ueno <hello@junueno.dev>
2023-03-17 22:09:41 +0900

    try out new get

--- complete/src/main/java/com/example/restservice/GreetingController.java
+++ complete/src/main/java/com/example/restservice/GreetingController.java
@@ -16,4 +16,9 @@ public class GreetingController {
 	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
+
+	@GetMapping("/users")
+	public Users users(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Users(counter.incrementAndGet(), String.format(template, name));
+	}
 }
---
+++ complete/src/main/java/com/example/restservice/Users.java
@@ -0,0 +1,3 @@
+package com.example.restservice;
+
+public record Users(long id, String content) { }
